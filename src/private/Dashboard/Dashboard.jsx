663b709ea7c5c8121d37891a5113deda909641d8
Maico Gabriel Zurbriggen <zurbriggenmaico8@gmail.com>
2025-06-17 22:52:21 -0300

    Continuando con el dashboard

--- src/private/Dashboard/Dashboard.jsx
+++ src/private/Dashboard/Dashboard.jsx
@@ -3,9 +3,12 @@ import { AppRoutes } from "../../models/routes.models";
 import { useFetch } from "../../hooks/useFetch";
 import { useState, useEffect } from "react";
 import { useAuth } from "../../context/AuthContext";
+import './Dashboard.css';
 
 export const Dashboard = () => {
   const [clothes, setClothes] = useState([]);
+  const [colors, setColors] = useState([]);
+  const [sizes, setSizes] = useState([]);
 
   const { token } = useAuth();
 
@@ -30,9 +33,45 @@ export const Dashboard = () => {
   const errorDeleteClothe = responseDeleteClothe.error;
   const refetch = responseDeleteClothe.refetch;
 
+  const responseColors = useFetch({ autoFetch: false });
+
+  let dataColors = responseColors.data;
+  let loadingColors = responseColors.loading;
+  let errorColors = responseColors.error;
+  const refetchColors = responseColors.refetch;
+
+  useEffect(() => {
+    dataColors = responseColors.data;
+  }, [responseColors]);
+
+  const responseSizes = useFetch({ autoFetch: false });
+
+  let dataSizes = responseSizes.data;
+  let loadingSizes = responseSizes.loading;
+  let errorSizes = responseSizes.error;
+  const refetchSizes = responseSizes.refetch;
+
+  useEffect(() => {
+    dataSizes = responseSizes.data;
+  }, [responseSizes]);
+
   useEffect(() => {
     setClothes(dataClothes);
   }, [dataClothes])
+
+  useEffect(() => {
+    console.log("Colores: ", colors);
+    if (dataColors) {
+      setColors(dataColors);
+    }
+  }, [dataColors]);
+
+  useEffect(() => {
+    console.log("Talles: ", sizes);
+    if (dataSizes) {
+      setSizes(dataSizes);
+    }
+  }, [dataSizes])
 
   const handleOpenEdit = () => {
     const modal = document.querySelector("#modalEdit");
@@ -45,7 +84,7 @@ export const Dashboard = () => {
   }
 
   const handleDelete = async (id) => {
-    refetch({
+    await refetch({
       url: `http://localhost:3000/api/prendas/${id}`,
       options: {
         method: "DELETE",
@@ -59,6 +98,34 @@ export const Dashboard = () => {
     if (!errorDeleteClothe) {
       setClothes(clothes.filter(clothe => clothe.id !== id));
     }
+  }
+
+  const handleCreate = async () => {
+    const panelCreate = document.querySelector('#panelCreate');
+
+    if (panelCreate !== "show") {
+      await refetchColors({
+        url: "http://localhost:3000/api/colores/",
+        options: {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      });
+
+      await refetchSizes({
+        url: "http://localhost:3000/api/talles/",
+        options: {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      });
+    }
+
+    panelCreate.classList.toggle('show');
   }
 
   if (loadingClothes) return <h1>Cargando...</h1>;
@@ -83,7 +150,7 @@ export const Dashboard = () => {
               <div>
                 <button onClick={handleOpenEdit}>Editar</button>
                 <button onClick={() => handleDelete(clothe.id)}>Eliminar</button>
-                <button>Crear</button>
+                <button onClick={handleCreate}>Crear</button>
               </div>
             </header>
             <main>
@@ -92,6 +159,32 @@ export const Dashboard = () => {
                 <button onClick={handleCloseEdit}>Cerrar</button>
                 <button>Guardar</button>
               </dialog>
+              <header id="panelCreate">
+                <h4>Crear Stocks</h4>
+                <div>
+                  <label htmlFor="cantidad">Cantidad:</label>
+                  <input type="number" id="cantidad" name="cantidad" min={1} />
+                </div>
+                <div>
+                  <label htmlFor="color">Color:</label>
+                  <select name="color" id="color">
+                    <option disabled defaultValue="">--Seleccione un color--</option>
+                    {colors.map((color, index) => (
+                      <option key={index} value={color.nombre}>{color.nombre}</option>
+                    ))}
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="talle">Talle:</label>
+                  <select name="talle" id="talle">
+                    <option defaultValue="" disabled>--Seleccione un talle--</option>
+                    {sizes.map((size, index) => (
+                      <option key={index} value={size.nombre}>{size.nombre}</option>
+                    ))}
+                  </select>
+                </div>
+                <button onClick={handleCreate}>Cerrar</button>
+              </header>
             </main>
           </div>
         ))}
